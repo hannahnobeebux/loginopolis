6cb88ca0b1dbb4b8486d436741acc2d293e6ad94
Hannah <115044113+hannahnobeebux@users.noreply.github.com>
2023-01-18 11:33:24 +0000

    completed + attempting extensions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require('bcrypt');
+// const { DataTypes } = require('sequelize/types');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -16,9 +18,101 @@ app.get('/', async (req, res, next) => {
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post("/register", async(req, res, next) => {
+  try {
+  //alternative - const{username, password} = req.body; 
+  const username = req.body.username; 
+  const password = req.body.password; 
+
+  const SALT_COUNT = 10; 
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT); 
+
+  const newUser = await User.create({username, password: hashedPassword})
+
+  if (newUser !== "") {
+    res.send(`successfully created user ${username}`);
+  } else {
+    return "Failed to create user"; 
+  }
+    
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+  
+
+})
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+
+app.post("/login", async(req, res, next) => {
+  try {
+    //alternative - const{username, password} = req.body; 
+    const username = req.body.username; 
+    const password = req.body.password; 
+
+    const hashedPassword = await bcrypt.hash(password, 10); 
+
+    const foundUser = await User.findOne({where: {username: username}}); 
+
+  
+    if (foundUser === null) {
+      res.status(401).send(`incorrect username or password`);
+    } else {
+      const passwordsMatch = await bcrypt.compare(password, foundUser.password); 
+      if(passwordsMatch) {
+        res.send(`successfully logged in user ${username}`);
+    } else {
+      res.status(401).send(`incorrect username or password`);
+    }
+  }
+  } catch (error) {
+    console.log(error);
+    //passing the error along so we can deal with it in an error handling method elsewhere 
+    next(error);
+  }
+})
+
+//EXTENSION - Hash the passwords for all users when creating them (SEEDED USERS)
+//Using something like Array.prototype.map() would be great here, calling bcrypt.hash for each password.
+//Try using Promise.all() to let each promise resolve in concurrency (more efficient).
+
+// async function hashingUserDetails (req,res)  {
+//   const allUsers = User.findAll()
+//   const hashedUserPasswords = allUsers.map(x => bcrypt.hash(x.password, 10))
+//   Promise.all([hashedUserPassword].then((values) => {
+//     console.log(values.password);
+//   }))
+// }
+
+//EXTENSION - Add a model like Post or Recipe or anything that can be associated to a User.
+//Create seed data for 2-3 of the users in the seed file.
+// function creatingNewModel(){
+
+//   class Post extends Model {}
+
+//   Post.init({
+//     title:{
+//       type: DataTypes.STRING, 
+//       allowNull: false 
+//     }, 
+//     description:{
+//       type: DataTypes.STRING, 
+//       allowNull: false
+//     }
+//   })
+
+//   User.hasMany(Post); 
+
+// }
+
+
+//EXTENSION - In other lessons, we’ll learn about better authentication & authorization flows, but for now:
+//Similar to how POST /login works, use the username and password to authenticate the user.
+//If the password matches, send back the associated data created in the last bonus step.
+
+
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
